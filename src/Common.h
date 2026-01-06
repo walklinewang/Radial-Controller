ab0c5399674b3362d6d958a7941972f245c8b00f
Walkline Wang <walkline@gmail.com>
2026-01-06 17:08:30 +0800

    refactor(Common.h): 移除调试代码并重命名EC11按键引脚
    移除DEBUG宏及相关调试打印代码，简化头文件
    将EC11_PIN_KEY重命名为EC11_PIN_K以保持命名一致性
    
    Signed-off-by: Walkline Wang <walkline@gmail.com>

--- src/Common.h
+++ src/Common.h
@@ -1,26 +1,12 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define DEBUG 1
-
 // EC11 编码器引脚定义
 #define EC11_PIN_A 12
 #define EC11_PIN_B 13
-#define EC11_PIN_KEY 15
+#define EC11_PIN_K 15
 
 // WS2812 引脚定义
 #define WS2812_PIN 16
-
-#if DEBUG
-#define DEBUG_PRINT(...) USBSerial_print(__VA_ARGS__)
-#define DEBUG_PRINTLN(...)                                                     \
-    do {                                                                       \
-        USBSerial_println(__VA_ARGS__);                                        \
-        USBSerial_flush();                                                     \
-    } while (0)
-#else
-#define DEBUG_PRINT(...)
-#define DEBUG_PRINTLN(...)
-#endif
 
 #endif /* __COMMON_H__ */
